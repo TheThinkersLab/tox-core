55e2c0204233df40be225bbf13564b3156c2dad3
Krishna Madhavan <krishnamadhavan07@gmail.com>
2024-11-10 20:25:27 +0530

    fix: add settings/common.py

--- tox_core/__init__.py
+++ tox_core/__init__.py
@@ -1,4 +1,4 @@
 """
 Init for main tox-core app
 """
-__version__ = '1.0.1'
+__version__ = '1.0.2'
---
+++ tox_core/settings/common.py
@@ -0,0 +1,9 @@
+"""
+Settings for eox-core
+"""
+from __future__ import absolute_import, unicode_literals
+
+from importlib.util import find_spec
+
+SECRET_KEY = 'a-not-to-be-trusted-secret-key'
+
